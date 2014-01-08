ea6cac4a381e8369306a513bbd6cda2f5871a31d
nobu42 <bast.achard@gmail.com>
2014-01-08 19:37:18 +0100

    separate js classes from the param, added comments

---
+++ roommate_manager/js/classes.js
@@ -0,0 +1,93 @@
+/**
+ * Draft of the dialog box
+ *
+ */
+
+function Box(id, width, height) {
+    
+    this.show = function() {
+	document.body.appendChild(this.element);
+    }
+    
+    this.hide = function() {
+	document.body.removeChild(this.element);
+    }
+    
+    this.refresh = function() {
+	
+	// we add the elements to the div 
+	this.element.appendChild(this.title);
+	this.element.appendChild(this.body);
+	this.element.appendChild('<section class="dialog_buttons"></section>');
+	
+	if(this.buttons) {
+	    var buttons_section = this.element.querySelector(".dialog_buttons");
+	    for(b in this.buttons) {
+		buttons_section.appendChild(this.buttons[b].element);
+	    }
+	}
+
+	// querySelector: selector with the same pattern as the css
+	// ex: <div id="test">
+	//       <span class="class">
+	// ...
+	// querySelector("#test .class") -> returns the span object
+	
+    }
+
+    this.setTitle = function(title) {
+	this.title = '<header class="dialog_header">'+title+'</header>';
+	this.refresh();
+    }
+    
+    this.setBody = function(body) {
+	this.body = '<section class="dialog_body">'+body+'</section>';
+	this.refresh();
+    }
+    
+    this.setWidth = function(width) {
+	this.element.style.width = width + 'px';
+    }
+
+    this.setHeight = function(height) {
+	this.element.style.height = height + 'px';
+    }
+
+    this.addButton = function(button) {
+	this.buttons[button.id] = button;
+	this.refresh();
+    }
+    
+    // we create a div in the html document
+    this.element = document.createElement('div');
+    
+    this.element.id = id;
+    this.setWidth(width);
+    this.setHeight(height);
+    //this.element.width = width + 'px';
+    //this.element.height = height + 'px';
+    this.element.className = 'dialog_box';
+    this.title = null;
+    this.body = null;
+    // object containing all the buttons of the box
+    // Button object
+    this.buttons = {};
+}
+
+/**
+ * Button class
+ *
+ */
+
+function Button(id, name) {
+    
+    this.id = id;
+    this.element = document.createElement('button');
+    this.element.id = id;
+    this.element.innerHTML = name;
+    
+    this.onclick = function(callback) {
+	this.element.addEventListener('click', callback);
+    }
+
+}
--- roommate_manager/js/script.js
+++ roommate_manager/js/script.js
@@ -1,102 +1,27 @@
+window.onload=function() { 
 
+    var dialog_box = new Box('box', 500, 300);
+    // We init the box
+    dialog_box.setTitle("Test");
+    dialog_box.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie erat vel felis viverra, in interdum eros sollicitudin.");
 
-/**
- * Draft of the dialog box
- *
- */
+    var but = new Button('test', 'test');
+    var but1 = new Button('test1', 'test');
+    var but2 = new Button('test2', 'test');
 
-function Box(id, width, height) {
-    
-    this.show = function() {
-	document.body.appendChild(this.element);
-    }
-    
-    this.hide = function() {
-	document.body.removeChild(this.element);
-    }
-    
-    this.refresh = function() {
-	this.element.innerHTML = 
-	    '<header class="dialog_header">'+this.title+'</header>'+
-	    '<section class="dialog_body">'+this.body+'</section>';
-	if(this.buttons) {
-	    this.element.innerHTML += '<section class="dialog_buttons"></section>';
-	    var buttons_section = this.element.querySelector(".dialog_buttons")
-	    for(b in this.buttons) {
-		buttons_section.appendChild(this.buttons[b].element);
-	    }
+    but.onclick(toggle_box);
+    but1.onclick(function() { alert(' ') });
+
+    dialog_box.addButton(but);
+    dialog_box.addButton(but1);
+    dialog_box.addButton(but2);
+
+    function toggle_box() {
+	if(!document.getElementById('box')) {
+	    dialog_box.show();
 	}
-    }
-
-    this.setTitle = function(title) {
-	this.title = title;
-	this.refresh();
-    }
-    
-    this.setBody = function(body) {
-	this.body = body;
-	this.refresh();
-    }
-    
-    this.setWidth = function(width) {
-	this.element.style.width = width + 'px';
-    }
-
-    this.setHeight = function(height) {
-	this.element.style.height = height + 'px';
-    }
-
-    this.addButton = function(button) {
-	this.buttons[button.id] = button;
-	this.refresh();
-    }
-    
-    this.element = document.createElement('div');
-    this.element.id = id;
-    this.setWidth(width);
-    this.setHeight(height);
-    //this.element.width = width + 'px';
-    //this.element.height = height + 'px';
-    this.element.className = 'dialog_box';
-    this.title = null;
-    this.body = null;
-    this.buttons = {};
-}
-
-function Button(id, name) {
-    
-    this.id = id;
-    this.element = document.createElement('button');
-    this.element.id = id;
-    this.element.innerHTML = name;
-    
-    this.prout = function(callback) {
-	alert(' ');
-	this.element.addEventListener('click', alert(' '));
-    }
-
-}
-
-var dialog_box = new Box('box', 500, 300);
-dialog_box.setTitle("Test");
-dialog_box.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie erat vel felis viverra, in interdum eros sollicitudin.");
-
-var but = new Button('test', 'test');
-var but1 = new Button('test1', 'test');
-var but2 = new Button('test2', 'test');
-
-but.prout(toggle_box);
-
-
-dialog_box.addButton(but);
-dialog_box.addButton(but1);
-dialog_box.addButton(but2);
-
-function toggle_box() {
-    if(!document.getElementById('box')) {
-	dialog_box.show();
-    }
-    else {
-	dialog_box.hide();
+	else {
+	    dialog_box.hide();
+	}
     }
 }
