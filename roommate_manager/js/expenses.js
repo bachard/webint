5c3930b5f60d50f69eddcea6f28356b371b035b7
Guillaume <guillaume.piatte@hotmail.fr>
2014-01-09 22:51:59 +0100

    prout

--- roommate_manager/js/expenses.js
+++ roommate_manager/js/expenses.js
@@ -2,8 +2,8 @@ window.onload=function() {
 
 var expense_box = new Box('box', document.getElementById('place_here'), 500, 100, 'expense_box', 'expense_header', 'expense_body', 'expense_buttons');
 
-    expense_box.setTitle("Add an expense");
-    expense_box.setBody("+20,25€");
+    expense_box.setTitle("Expenses details");
+    expense_box.setBody("Food: 20,25€ 05/11/2013");
 
     var but3 = new Button('test', 'quit');
 
@@ -13,4 +13,5 @@ var expense_box = new Box('box', document.getElementById('place_here'), 500, 100
     
     document.getElementById('expense_box').addEventListener('click', function() { expense_box.show() });
 
+    /*document.getElementById('box').style.height=100+'px';*/
 }
