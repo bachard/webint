d691d6d6e291ea53f0ef76c7574beca234fb78ba
nobu42 <bast.achard@gmail.com>
2014-01-12 15:06:12 +0100

    work on notifs

--- roommate_manager/js/script.js
+++ roommate_manager/js/script.js
@@ -8,29 +8,29 @@ window.onload=function() {
     document.body.appendChild(divanim);
     */
     // we create the box
-    var dialog_box = new Box('box', document.body, 500, 300);
+    // var dialog_box = new Box('box', document.body, 500, 300);
     // we init the box
-    dialog_box.setTitle("Nice box");
+    // dialog_box.setTitle("Nice box");
     // dialog_box.setBody("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie erat vel felis viverra, in interdum eros sollicitudin.");
     // how to import external html directly in the box body
     
-    dialog_box.importHTML("source/table.html"); 
+    // dialog_box.importHTML("source/table.html"); 
     // we create the buttons for the box
-    var but = new Button('test', 'quit');
-    var but1 = new Button('test1', 'test');
-    var but2 = new Button('test2', 'test');
+    //var but = new Button('test', 'quit');
+    //var but1 = new Button('test1', 'test');
+    //var but2 = new Button('test2', 'test');
     // we bind the events
-    but.onclick(function() { dialog_box.hide() });
-    but1.onclick(function() { alert(' ') });
+    //but.onclick(function() { dialog_box.hide() });
+    //but1.onclick(function() { alert(' ') });
     // we add the buttons to the box
-    dialog_box.addButton(but);
-    dialog_box.addButton(but1);
-    dialog_box.addButton(but2);
+    //dialog_box.addButton(but);
+    //dialog_box.addButton(but1);
+    //dialog_box.addButton(but2);
     /* end of example */
-
+    //
     // ex. of how to bind an event to the onclick event of a button defined in html
     // and not in the js file
-    document.getElementById('add_expense').addEventListener('click', function() { dialog_box.open(); dialog_box.show(); });
+    //document.getElementById('add_expense').addEventListener('click', function() { dialog_box.open(); dialog_box.show(); });
 
     /*
      * Test de drag and drop sur une image
@@ -60,5 +60,5 @@ window.onload=function() {
 	document.body.appendChild(this);
     });
     */
-    
+    initialize();
 }
